586325d3598c7b103669d3aee66f1ea8f05bef77
Aleksander Repin <alex.repin@mail.ru>
2019-11-26 22:02:49 +0300

    0.0.4

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const notWSServer = require('./src/node/server');
+const notWSServerClient = require('./src/node/server.client');
+const notWSRouter = require('./src/node/router');
+const notWSMessenger = require('./src/node/messenger');
+const notWSClient = require('./src/node/client');
+const CONST = require('./src/node/const');
+const LOG = require('./src/node/log');
+
 module.exports = {
 	name: 'not-ws',
-	paths: {		
-		controllers:	path.join(__dirname, 'src', 'controllers'),
-		locales:			path.join(__dirname, 'src', 'locales')
-	}
+	paths: {
+		controllers:	path.join(__dirname, 'src', 'controllers')
+	},
+	notWSServer,
+	notWSServerClient,
+	notWSRouter,
+	notWSMessenger,
+	notWSClient,
+	CONST,
+	LOG
 };
