8bea966cde3ae16f488be554d9e3bf9a0d9d4553
interrupter <alex.repin@mail.ru>
2021-05-31 04:51:01 +0300

    updated: front service adjusted to new version of notWSClient

--- src/controllers/common/service.js
+++ src/controllers/common/service.js
@@ -15,35 +15,29 @@ class nsWS {
 
 	initClient(name, opts){
 		try{
-			let options = {}, optionsFromAppConfig = false;
-			//copy opts from app wide config
-			optionsFromAppConfig = this.app.getOptions(`modules.ws.clients.${name}`, false);
-			if(optionsFromAppConfig){
-				options.options = Object.assign({}, {...optionsFromAppConfig});
-			}
-			if(Object.prototype.hasOwnProperty.call(opts, 'routes')){
-				options.options.routes = opts.routes;
-			}
+			let options = {},
+				optionsFromAppConfig = false;
 			//integrating with collected from modules
-			for(let optName in ['options', 'messenger', 'router']){
+			for(let optName of ['name', 'messenger', 'router', 'getToken', 'logger', 'debug']){
 				if(Object.prototype.hasOwnProperty.call(opts, optName)){
 					options[optName] = opts[optName];
 				}
 			}
-
+			//copy opts from app wide config
+			optionsFromAppConfig = this.app.getOptions(`modules.ws.clients.${name}`, false);
+			if(optionsFromAppConfig){
+				if(!Object.prototype.hasOwnProperty.call(options, 'connection')){
+					options.connection = {};
+				}
+				options.connection = Object.assign(options.connection, {...optionsFromAppConfig});
+			}
 			let client = new notWSClient(options);
 			this.app.setWSClient(name, client);
-			if(Object.prototype.hasOwnProperty.call(opts, 'routes')){
-				if(Object.prototype.hasOwnProperty.call(opts.routes, 'event')){
-					for(let evName in opts.routes.event){
-						client.on(`remote.${evName}`, opts.routes.event[evName]);
-					}
-				}
-			}
 		}catch(e){
 			this.app.error(e);
 		}
 	}
+
 }
 
 export default nsWS;
