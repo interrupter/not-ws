27b1e334fe8bd2ac998db3ff7878116a020e4076
interrupter <alex.repin@mail.ru>
2021-06-03 22:12:39 +0300

    added: front-end service notApp events for client connected/disconnected/ready

--- src/node/func.js
+++ src/node/func.js
@@ -1,50 +1,50 @@
 //Проверка является ли переменная функцией.
 function isFunc(func){
-	return typeof(func) === 'function';
+  return typeof(func) === 'function';
 }
 
 function noop() {}
 function heartbeat() { this._alive = true;}
 
 function ObjHas(obj, prop){
-	return Object.prototype.hasOwnProperty.call(obj, prop);
+  return Object.prototype.hasOwnProperty.call(obj, prop);
 }
 
 //Проверка строки на JSON(со stackoverflow).
 //http://stackoverflow.com/questions/3710204/how-to-check-if-a-string-is-a-valid-json-string-in-javascript-without-using-try
 let tryParseJSON = function (jsonString){
-	try {
-		let o = JSON.parse(jsonString);
-		// Handle non-exception-throwing cases:
-		// Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
-		// but... JSON.parse(null) returns null, and typeof null === "object",
-		// so we must check for that, too. Thankfully, null is falsey, so this suffices:
-		if (o && typeof o === "object") {
-			return o;
-		}
-	}catch (e) {
-		// eslint-disable-next-line no-console
-		console.error(e);
-	}
-	return false;
+  try {
+    let o = JSON.parse(jsonString);
+    // Handle non-exception-throwing cases:
+    // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+    // but... JSON.parse(null) returns null, and typeof null === "object",
+    // so we must check for that, too. Thankfully, null is falsey, so this suffices:
+    if (o && typeof o === "object") {
+      return o;
+    }
+  }catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
+  return false;
 };
 
 function isArray(val){
-	return Array.isArray(val);
+  return Array.isArray(val);
 }
 
 let capitalizeFirstLetter = function(name){
-	return name.charAt(0).toUpperCase() + name.slice(1);
+  return name.charAt(0).toUpperCase() + name.slice(1);
 };
 
 
-module.exports = {
-	isFunc,
-	isArray,
-	noop,
-	heartbeat,
-	ObjHas,
-	tryParseJSON,
-	capitalizeFirstLetter
-};
+  module.exports = {
+    isFunc,
+    isArray,
+    noop,
+    heartbeat,
+    ObjHas,
+    tryParseJSON,
+    capitalizeFirstLetter
+  };
 
--- src/node/server.js
+++ src/node/server.js
@@ -9,16 +9,16 @@ const Func = require('./func.js');
 const notWSClient = require('./client.js');
 
 const DEFAULT_CONNECTION = {
-	port: 12000,
-	ping: true,
-	secure: true,
-	//ретрансляция сообщений
-	relay: null,
+  port: 12000,
+  ping: true,
+  secure: true,
+  //ретрансляция сообщений
+  relay: null,
 };
 
 class notWSServer extends EventEmitter{
 
-	/**
+  /**
   *  connection           -   connection params
   *  connection.server    -   express http/s server
   *  connection.port      -   server port
@@ -35,237 +35,237 @@ class notWSServer extends EventEmitter{
   *  credentials          -   creds
   **/
 
-	constructor({
-		connection = {},
-		jwt,
-		getRouter,
-		routes,
-		getMessenger,
-		messenger,
-		credentials,
-		logger
-	}){
-		if(!Func.isFunc(getRouter) && !routes){
-			throw new Error('getRouter is not function and routes is not set');
-		}
-		if(!Func.isFunc(getMessenger) && !messenger){
-			throw new Error('getMessenger is not function and messenger is not set');
-		}
-		super();
-		this.connection = Object.assign({}, DEFAULT_CONNECTION, connection);
+  constructor({
+      connection = {},
+      jwt,
+      getRouter,
+      routes,
+      getMessenger,
+      messenger,
+      credentials,
+      logger
+    }){
+    if(!Func.isFunc(getRouter) && !routes){
+      throw new Error('getRouter is not function and routes is not set');
+    }
+    if(!Func.isFunc(getMessenger) && !messenger){
+      throw new Error('getMessenger is not function and messenger is not set');
+    }
+    super();
+    this.connection = Object.assign({}, DEFAULT_CONNECTION, connection);
 
-		this.logMsg = logger?logger.log:()=>{};
+    this.logMsg = logger?logger.log:()=>{};
 		this.logDebug = logger?logger.debug:()=>{};
 		this.logError = logger?logger.error:()=>{};
 
-		this.routes = routes;
-		this.messenger = messenger;
+    this.routes = routes;
+    this.messenger = messenger;
 
-		this.wsServer   = null;
-		this.isClosing   = false;
-		this.wsClients  = [];
-		this.wsMaxConns = 100;
+    this.wsServer   = null;
+    this.isClosing   = false;
+    this.wsClients  = [];
+    this.wsMaxConns = 100;
 
-		this.jwt = jwt;
-		this.credentials = credentials;
+    this.jwt = jwt;
+    this.credentials = credentials;
 
-		this.getRouter = getRouter;
-		this.getMessenger = getMessenger;
+    this.getRouter = getRouter;
+    this.getMessenger = getMessenger;
 
-		this.intPingPong = null;
-		return this;
-	}
+    this.intPingPong = null;
+    return this;
+  }
 
-	start(){
-		let opts = {};
-		if(this.connection.server){
-			opts.server = this.connection.server;
-		}
-		if(!isNaN(this.connection.port) && this.connection.port){
-			opts.port = this.connection.port;
-		}
-		if(this.connection.host){
-			opts.host = this.connection.host;
-		}
-		if(opts.host || opts.port || opts.server){
-			this.wsServer = new ws.Server(opts);
-			if(this.connection.ping){
-				this.initPingPong();
-			}
-			this.initServer();
-			this.emit('started');
-		}else{
-			throw new Error('No host, port or server object for start.');
-		}
-	}
+  start(){
+    let opts = {};
+    if(this.connection.server){
+       opts.server = this.connection.server;
+    }
+    if(!isNaN(this.connection.port) && this.connection.port){
+      opts.port = this.connection.port;
+    }
+    if(this.connection.host){
+      opts.host = this.connection.host;
+    }
+    if(opts.host || opts.port || opts.server){
+      this.wsServer = new ws.Server(opts);
+      if(this.connection.ping){
+        this.initPingPong();
+      }
+      this.initServer();
+      this.emit('started');
+    }else{
+      throw new Error('No host, port or server object for start.');
+    }
+  }
 
-	async stop(){
-		try{
-			this.setClosing();
-			await this.closeClientConnections();
-			await this.closeServer();
-		}catch(e){
-			this.logError(e);
-		}
-	}
+  async stop(){
+    try{
+      this.setClosing();
+      await this.closeClientConnections();
+      await this.closeServer();
+    }catch(e){
+      this.logError(e);
+    }
+  }
 
-	initServer(){
-		this.wsServer.on('error', this.onError.bind(this));
-		this.wsServer.on('connection', this.onConnection.bind(this));
-	}
+  initServer(){
+    this.wsServer.on('error', this.onError.bind(this));
+    this.wsServer.on('connection', this.onConnection.bind(this));
+  }
 
-	onError(err){
-		this.logError(`ws server error: `, err);
-		this.emit('wsError', err);
-	}
+  onError(err){
+    this.logError(`ws server error: `, err);
+    this.emit('wsError', err);
+  }
 
-	isSecure(){
-		return !!this.connection.secure;
-	}
+  isSecure(){
+    return !!this.connection.secure;
+  }
 
-	connectionIsNotSecure(conn, req){
-		//Пропускаем уже отключенные токены.
-		if((!conn) || (conn.readyState !== conn.OPEN)){
-			return false;
-		}
-		let token = url.parse(req.url, true).query.token;
-		//Проверяем токен
-		try {
-			this.logMsg('token', token);
-			let decoded  =  jwt.verify(token, this.jwt.key);
-			this.logMsg('decoded token', decoded);
-			return !decoded.active;
-		} catch(err) {
-			this.logDebug(err);
-			if(err.name === 'TokenExpiredError'){
-				this.logError('Client must update token');
-			}
-			return true;
-		}
-	}
+  connectionIsNotSecure(conn, req){
+    //Пропускаем уже отключенные токены.
+    if((!conn) || (conn.readyState !== conn.OPEN)){
+      return false;
+    }
+    let token = url.parse(req.url, true).query.token;
+    //Проверяем токен
+    try {
+      this.logMsg('token', token);
+      let decoded  =  jwt.verify(token, this.jwt.key);
+      this.logMsg('decoded token', decoded);
+      return !decoded.active;
+    } catch(err) {
+      this.logDebug(err);
+      if(err.name === 'TokenExpiredError'){
+        this.logError('Client must update token');
+      }
+      return true;
+    }
+  }
 
-	getClientIdentity(req){
-		if(this.isSecure()){
-			let token = url.parse(req.url, true).query.token;
-			return jwt.verify(token, this.jwt.key);
-		}else{
-			return false;
-		}
-	}
+  getClientIdentity(req){
+    if(this.isSecure()){
+      let token = url.parse(req.url, true).query.token;
+      return jwt.verify(token, this.jwt.key);
+    }else{
+      return false;
+    }
+  }
 
-	onConnection(connection, req){
-		if(this.isSecure()){
-			this.logMsg('Secure server');
-			if(this.connectionIsNotSecure(connection, req)){
-				this.informClientAboutExperiedToken(connection, req)
+  onConnection(connection, req){
+    if(this.isSecure()){
+      this.logMsg('Secure server');
+      if(this.connectionIsNotSecure(connection, req)){
+        this.informClientAboutExperiedToken(connection, req)
 					.then(()=>{
 						connection.close();
 					})
 					.catch(this.logError.bind(this));
 				this.logError( new Error(`Connection from ${req.socket.remoteAddress}/${req.url} refused, as not secure`));
 				return;
-			}
-		}
-		let wsConn = new notWSClient({
-			identity:     this.getClientIdentity(req),
-			slave:        true,
-			connection:{
-				ping:         false,
-				ws:           connection,
-				ip:           req.socket.remoteAddress,
-			},
-			credentials:  Object.assign({}, this.credentials),
-			messenger:    this.getMessengerForClient(connection, req),
-			router:       this.getRouterForClient(connection, req)
-		});
-		this.emit('connection', wsConn, req);
-		wsConn.connection.once('errored', this.terminateAndRemoveWSClient.bind(this));
-		wsConn.on('message', this.onClientMessage.bind(this));
-		this.wsClients.push(wsConn);
-		this.logMsg(`New client from ${req.socket.remoteAddress}`);
-		this.lastCount = this.wsClients.length;
-		this.emit('clientsCountChanged', this.wsClients.length);
-	}
+      }
+    }
+    let wsConn = new notWSClient({
+      identity:     this.getClientIdentity(req),
+      slave:        true,
+      connection:{
+        ping:         false,
+        ws:           connection,
+        ip:           req.socket.remoteAddress,
+      },
+      credentials:  Object.assign({}, this.credentials),
+      messenger:    this.getMessengerForClient(connection, req),
+      router:       this.getRouterForClient(connection, req)
+    });
+    this.emit('connection', wsConn, req);
+    wsConn.connection.once('errored', this.terminateAndRemoveWSClient.bind(this));
+    wsConn.on('message', this.onClientMessage.bind(this));
+    this.wsClients.push(wsConn);
+    this.logMsg(`New client from ${req.socket.remoteAddress}`);
+    this.lastCount = this.wsClients.length;
+    this.emit('clientsCountChanged', this.wsClients.length);
+  }
 
-	onClientMessage(){
-		this.emit('clientMessage', ...arguments);
-	}
+  onClientMessage(){
+    this.emit('clientMessage', ...arguments);
+  }
 
-	getMessengerForClient(conn, req){
-		if(Func.isFunc(this.getMessenger)){
-			return this.getMessenger(conn, req, this.messenger);
-		}else{
-			return this.messenger;
-		}
-	}
+  getMessengerForClient(conn, req){
+    if(Func.isFunc(this.getMessenger)){
+      return this.getMessenger(conn, req, this.messenger);
+    }else{
+      return this.messenger;
+    }
+  }
 
-	getRouterForClient(conn, req){
-		if(Func.isFunc(this.getRouter)){
-			return this.getRouter(conn, req, this.routes);
-		}else{
-			return {routes: this.routes};
-		}
-	}
+  getRouterForClient(conn, req){
+    if(Func.isFunc(this.getRouter)){
+      return this.getRouter(conn, req, this.routes);
+    }else{
+      return {routes: this.routes};
+    }
+  }
 
-	setClosing(){
-		this.isClosing = true;
-	}
+  setClosing(){
+    this.isClosing = true;
+  }
 
-	closeServer(){
-		return new Promise((res, rej)=>{
-			try{
-				this.wsServer.close((e)=>{
-					if(e){
-						rej(e);
-					}else{
-						res();
-					}
-				});
-			}catch(e){
-				rej(e);
-			}
-		});
-	}
+  closeServer(){
+    return new Promise((res, rej)=>{
+      try{
+        this.wsServer.close((e)=>{
+          if(e){
+            rej(e);
+          }else{
+            res();
+          }
+        });
+      }catch(e){
+        rej(e);
+      }
+    });
+  }
 
-	closeClientConnections(){
-		while(this.wsClients.length){
-			if(this.wsClients[0].isConnected()){
-				this.terminateAndRemoveWSClient(this.wsClients[0]);
-			}else{
-				this.removeClient(this.wsClients[0]);
-			}
-		}
-	}
+  closeClientConnections(){
+    while(this.wsClients.length){
+      if(this.wsClients[0].isConnected()){
+        this.terminateAndRemoveWSClient(this.wsClients[0]);
+      }else{
+        this.removeClient(this.wsClients[0]);
+      }
+    }
+  }
 
-	terminateAndRemoveWSClient(wsc){
-		if (wsc){
-			wsc.terminate();
-			wsc.destroy();
-			this.removeClient(wsc);
-		}
-	}
+  terminateAndRemoveWSClient(wsc){
+    if (wsc){
+      wsc.terminate();
+      wsc.destroy();
+      this.removeClient(wsc);
+    }
+  }
 
-	removeClient(wsc){
-		if(this.wsClients.indexOf(wsc) > -1){
-			this.wsClients.splice(this.wsClients.indexOf(wsc), 1);
-		}
-	}
+  removeClient(wsc){
+    if(this.wsClients.indexOf(wsc) > -1){
+      this.wsClients.splice(this.wsClients.indexOf(wsc), 1);
+    }
+  }
 
-	findAndRemoveDeadClients(){
-		let dead =[];
-		for(let client of this.wsClients){
-			if(client.isDead()){
-				dead.push(client);
-			}
-		}
-		dead.forEach(this.removeClient.bind(this));
-		if(this.lastCount !== this.wsClients.length) {
-			this.lastCount = this.wsClients.length;
-			this.emit('clientsCountChanged', this.wsClients.length);
-		}
-	}
+  findAndRemoveDeadClients(){
+    let dead =[];
+    for(let client of this.wsClients){
+      if(client.isDead()){
+        dead.push(client);
+      }
+    }
+    dead.forEach(this.removeClient.bind(this));
+    if(this.lastCount !== this.wsClients.length) {
+      this.lastCount = this.wsClients.length;
+      this.emit('clientsCountChanged', this.wsClients.length);
+    }
+  }
 
-	initPingPong(){
+  initPingPong(){
 		if(this.intPingPong){
 			clearInterval(this.intPingPong);
 		}
@@ -275,80 +275,80 @@ class notWSServer extends EventEmitter{
 
 	pingAll(){
 		this.wsClients.forEach(this.pingOne.bind(this));
-		this.findAndRemoveDeadClients();
+    this.findAndRemoveDeadClients();
 	}
 
 	pingOne(client){
 		client.ping();
 	}
 
-	informClientAboutExperiedToken(conn, req){
-		return new Promise((res, rej)=>{
-			try{
-				let msg = {
-					type:'__service',
-					name: 'updateToken'
-				};
-				conn.send(JSON.stringify(this.getMessengerForClient(conn, req).pack({}, msg)),
-					(e)=>{
-						if(e){rej(e);}
-						else{res();}
-					});
-			}catch(e){
-				rej(e);
-			}
-		});
-	}
+  informClientAboutExperiedToken(conn, req){
+    return new Promise((res, rej)=>{
+    try{
+      let msg = {
+        type:'__service',
+        name: 'updateToken'
+      };
+      conn.send(JSON.stringify(this.getMessengerForClient(conn, req).pack({}, msg)),
+        (e)=>{
+          if(e){rej(e);}
+          else{res();}
+        });
+    }catch(e){
+      rej(e);
+    }
+    });
+  }
 
-	/**
+  /**
   * Broadcasting message to clients
   * @param {string} type  type of the message
   * @param {string} name  name of the message
   * @param {object} payload data to be transmitted
   * @secure {boolean} secure
   */
-	broadcast(type, name, payload, secure = true, connFilter = undefined){
-		this.getClients(connFilter).forEach((client)=>{
-			client.send(type, name, payload, secure);
-		});
-	}
+  broadcast(type, name, payload, secure = true, connFilter = undefined){
+    this.getClients(connFilter).forEach((client)=>{
+      client.send(type, name, payload, secure);
+    });
+  }
 
-	/**
+  /**
   * filtering clients by specified function and return resulting array
   * @param {function} filter function to filter clients
   * @returns {Array}
   */
-	getClients(filter){
-		if(Func.isFunc(filter)){
-			return this.wsClients.filter(filter);
-		}else{
-			return this.wsClients;
-		}
-	}
+  getClients(filter){
+    if(Func.isFunc(filter)){
+      return this.wsClients.filter(filter);
+    }else{
+      return this.wsClients;
+    }
+  }
 
-	/**
+  /**
   * filtering clients by specified function and return first passing test
   * @param {function} test function to test clients and found required
   * @returns {notWSServerClient}
   */
-	getClient({test, _id}){
-		if(!Func.isFunc(test) && _id){
-			test = (client)=>{
-				if(client.identity){
-					if(client.identity && client.identity._id){
-						return client.identity._id.toString() === _id.toString();
-					}
-				}
-				return false;
-			};
-		}
-		for(let t in this.wsClients){
-			if(test(this.wsClients[t])){
-				return this.wsClients[t];
-			}
-		}
-		return false;
-	}
+  getClient({test, _id}){
+    if(!Func.isFunc(test) && _id){
+      test = (client)=>{
+        if(client.identity){
+          if(client.identity && client.identity._id){
+            return client.identity._id.toString() === _id.toString();
+          }
+        }
+        return false;
+      }
+    }
+    for(let t in this.wsClients){
+      if(test(this.wsClients[t])){
+        return this.wsClients[t];
+      }
+    }
+    return false;
+  }
 
 }
 module.exports = notWSServer;
