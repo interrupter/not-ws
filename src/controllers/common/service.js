6c9169dca8f19b909a72fe828b8a70eacd7b4679
interrupter <alex.repin@mail.ru>
2023-03-22 23:00:20 +0300

    added: nsWS binding to App event token//updated to update tokens in clients and reconnect them to servers

--- src/controllers/common/service.js
+++ src/controllers/common/service.js
@@ -1,54 +1,96 @@
-import notWSClient from '../../browser/client.js';
+import notWSClient from "../../browser/client.js";
 
 class nsWS {
-	constructor(app) {
-		this.app = app;
-		this.init();
-	}
+    constructor(app) {
+        this.app = app;
+        this.init();
+        app.on("token//updated", () => this.getTokenFromApp());
+    }
 
-	init(){
-		const clients = this.app.getOptions('wsc', {});
-		for(let client in clients){
-			this.initClient(client, clients[client]);
-		}
-		this.app.on('user.logout', this.clearMainToken.bind(this));
-	}
+    getTokenFromApp() {
+        const clients = this.app.getOptions("wsc", {});
+        for (let client in clients) {
+            this.app
+                .getWSClient(client)
+                .saveToken(this.app.getWorking("token"));
+            this.app.getWSClient(client).disconnect();
+            this.app.getWSClient(client).connect().catch(app.report);
+        }
+    }
 
-	initClient(name, opts){
-		try{
-			let options = {},
-				optionsFromAppConfig = false;
-			//integrating with collected from modules
-			for(let optName of ['name', 'messenger', 'router', 'getToken', 'logger', 'debug']){
-				if(Object.prototype.hasOwnProperty.call(opts, optName)){
-					options[optName] = opts[optName];
-				}
-			}
-			//copy opts from app wide config
-			optionsFromAppConfig = this.app.getOptions(`modules.ws.clients.${name}`, false);
-			if(optionsFromAppConfig){
-				if(!Object.prototype.hasOwnProperty.call(options, 'connection')){
-					options.connection = {};
-				}
-				options.connection = Object.assign(options.connection, {...optionsFromAppConfig});
-			}
-			let client = new notWSClient(options);
-			client.once('ready', this.app.emit.bind(this.app, `wsClient:${name}:ready`, client));
-			client.on('connected', this.app.emit.bind(this.app, `wsClient:${name}:connected`, client));
-			client.on('diconnected', this.app.emit.bind(this.app, `wsClient:${name}:diconnected`, client));
-			this.app.setWSClient(name, client);
-		}catch(e){
-			this.app.error(e);
-		}
-	}
+    init() {
+        const clients = this.app.getOptions("wsc", {});
+        for (let client in clients) {
+            this.initClient(client, clients[client]);
+        }
+        this.app.on("user.logout", this.clearMainToken.bind(this));
+    }
 
-	clearMainToken(){
-		const client = this.app.getWSClient('main');
-		if(client){
-			client.clearToken();
-		}
-	}
+    initClient(name, opts) {
+        try {
+            let options = {},
+                optionsFromAppConfig = false;
+            //integrating with collected from modules
+            for (let optName of [
+                "name",
+                "messenger",
+                "router",
+                "getToken",
+                "logger",
+                "debug",
+            ]) {
+                if (Object.prototype.hasOwnProperty.call(opts, optName)) {
+                    options[optName] = opts[optName];
+                }
+            }
+            //copy opts from app wide config
+            optionsFromAppConfig = this.app.getOptions(
+                `modules.ws.clients.${name}`,
+                false
+            );
+            if (optionsFromAppConfig) {
+                if (
+                    !Object.prototype.hasOwnProperty.call(options, "connection")
+                ) {
+                    options.connection = {};
+                }
+                options.connection = Object.assign(options.connection, {
+                    ...optionsFromAppConfig,
+                });
+            }
+            let client = new notWSClient(options);
+            client.once(
+                "ready",
+                this.app.emit.bind(this.app, `wsClient:${name}:ready`, client)
+            );
+            client.on(
+                "connected",
+                this.app.emit.bind(
+                    this.app,
+                    `wsClient:${name}:connected`,
+                    client
+                )
+            );
+            client.on(
+                "diconnected",
+                this.app.emit.bind(
+                    this.app,
+                    `wsClient:${name}:diconnected`,
+                    client
+                )
+            );
+            this.app.setWSClient(name, client);
+        } catch (e) {
+            this.app.error(e);
+        }
+    }
 
+    clearMainToken() {
+        const client = this.app.getWSClient("main");
+        if (client) {
+            client.clearToken();
+        }
+    }
 }
 
 export default nsWS;
